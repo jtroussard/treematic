eece7139114e67fe3a4406edc190fae9a60e0b3e
Jacques Troussard <mm_sparrow@beeswaxnoneofyours.com>
2024-03-03 13:01:32 -0500

    [FIX] fixes bundling issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  target: 'webworker',
+  target: 'node',
   entry: './src/extension.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
